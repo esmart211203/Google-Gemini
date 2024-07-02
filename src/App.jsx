8cbd7067176a47c55fed358b32d70410c43579a1
esmart211203 <esmart211203@gmail.com>
2024-07-02 16:49:13 +0700

    added project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Sidebar from './components/Sidebar/sidebar'
+import Main from './components/Main/main'
+
+function App() {
+
+  return (
+    <>
+    <Sidebar/>
+    <Main/>
+    </>
+  )
+}
+
+export default App
